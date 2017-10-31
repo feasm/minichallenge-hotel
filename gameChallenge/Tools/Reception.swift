7d32288a59a48a07387328bc5bbd9d625299a02f
Unknown <contato@adoonay.com>
2017-10-31 14:35:09 -0200

    reception comit

--- gameChallenge/Tools/Reception.swift
+++ gameChallenge/Tools/Reception.swift
@@ -1,50 +1,22 @@
 //
-//  Teleporter.swift
+//  Reception.swift
 //  gameChallenge
 //
-//  Created by Adonay Puszczynski on 30/10/17.
+//  Created by Adonay Puszczynski on 31/10/17.
 //  Copyright © 2017 Adonay Puszczynski. All rights reserved.
 //
 
 import UIKit
 
-protocol TeleporterDelegate
+protocol ReceptionDelegate
 {
-    func choseFloor(floor: Int)
+    func sendToRoom(guest: Guest, room: Int)
 }
 
-class TeleporterCell : UICollectionViewCell
+class ReceptionCell : UICollectionViewCell
 {
-    var floorName : String = ""
-    var floorID : Int?
-    
-    let floorText : UILabel =
-    {
-        let label = UILabel()
-        label.translatesAutoresizingMaskIntoConstraints = false
-        label.font = UIFont.systemFont(ofSize: 20)
-        label.textColor = UIColor.white
-        return label
-    }()
-    
     override init(frame: CGRect) {
         super.init(frame: frame)
-        layer.cornerRadius = frame.size.height/2
-        backgroundColor = UIColor.brown
-        setupViews()
-    }
-    
-    func setFloor(floorName: String, floorID: Int)
-    {
-        self.floorID = floorID
-        self.floorName = floorName
-        self.floorText.text = floorName
-    }
-    
-    func setupViews() {
-        addSubview(floorText)
-        floorText.centerXAnchor.constraint(equalTo: centerXAnchor).isActive = true
-        floorText.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -52,14 +24,10 @@ class TeleporterCell : UICollectionViewCell
     }
 }
 
-class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
+class Reception: UIView, UICollectionViewDelegate, UICollectionViewDataSource, UICollectionViewDelegateFlowLayout
 {
-    let buttonSize = CGSize(width: 50, height: 50)
-    let reuseIdentifier = "cellTeleporter"
-    var delegate : TeleporterDelegate?
-    var selectedFloor : Int = -10
-    
-    var floors : [String : Int] = [:]
+    let reuseIdentifier = "cellReception"
+    var delegate : ReceptionDelegate?
     
     var collectionView : UICollectionView =
     {
@@ -75,10 +43,6 @@ class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource,
         alpha = 0
         backgroundColor = .blue
         setupViews()
-        
-        floors["0"] = 0
-        floors["1"] = 1
-        floors["2"] = 2
     }
     
     required init?(coder aDecoder: NSCoder) {
@@ -90,20 +54,21 @@ class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource,
         addSubview(collectionView)
         collectionView.dataSource = self
         collectionView.delegate = self
-        collectionView.register(TeleporterCell.self, forCellWithReuseIdentifier: reuseIdentifier)
+        collectionView.register(ReceptionCell.self, forCellWithReuseIdentifier: reuseIdentifier)
         collectionView.backgroundColor = UIColor.red
-        collectionView.rightAnchor.constraint(equalTo: rightAnchor, constant: -30).isActive = true
+        
+        collectionView.leftAnchor.constraint(equalTo: leftAnchor, constant: 30).isActive = true
         collectionView.centerYAnchor.constraint(equalTo: centerYAnchor).isActive = true
-        collectionView.widthAnchor.constraint(equalToConstant: 175).isActive = true
+        collectionView.widthAnchor.constraint(equalTo: widthAnchor, multiplier: 0.6).isActive = true
         collectionView.heightAnchor.constraint(equalTo: heightAnchor, multiplier: 0.8).isActive = true
     }
     
-    func showTeleporter()
+    func showReception()
     {
         self.bringSubview(toFront: self)
         if GameModel.DEBUG
         {
-            print("Showing teleporter")
+            print("Showing reception")
         }
         self.alpha = 0
         UIView.animate(withDuration: 1) {
@@ -111,7 +76,7 @@ class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource,
         }
     }
     
-    func hideTeleporter()
+    func hideReception()
     {
         self.alpha = 1
         UIView.animate(withDuration: 1) {
@@ -124,11 +89,11 @@ class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return floors.count
+        return 5
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        return buttonSize
+        return CGSize(width: 120, height: 250)
     }
     
     func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
@@ -140,20 +105,14 @@ class Teleporter : UIView, UICollectionViewDelegate, UICollectionViewDataSource,
     }
     
     func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! TeleporterCell
-        let value = Array(floors.keys)[indexPath.item]
-        cell.setFloor(floorName: value, floorID: floors[value]!)
+        let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath) as! ReceptionCell
         cell.backgroundColor = UIColor.black
         return cell
     }
     
     func collectionView(_ collectionView: UICollectionView, didSelectItemAt indexPath: IndexPath)
     {
-        let cell = collectionView.cellForItem(at: indexPath) as! TeleporterCell
-        if cell.floorID != nil
-        {
-            self.delegate?.choseFloor(floor: cell.floorID!)
-        }
-        self.hideTeleporter()
+        //let cell = collectionView.cellForItem(at: indexPath) as! ReceptionCell
+        self.hideReception()
     }
 }
